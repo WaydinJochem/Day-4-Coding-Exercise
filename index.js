0b4d49d06db090cf41f8de989bd4ce8bc15d4b2c
WaydinJochem <jochem7405@gmail.com>
2023-05-18 09:31:15 +0200

    Day-4

---
+++ index.js
@@ -0,0 +1,11 @@
+let userOne = { Name: 'Waydin', Surname: 'Jcohem', Age: '20', Occupation: 'full-time' };
+let userTwo = {
+    name: 'Jeff',
+    surname: 'Jefferson',
+    age: '27',
+    occupation: 'part-time'
+};
+console.log (userOne);
+console.log (userTwo);
+let people = ['userOne', 'userTwo'];
+console.log (people);
